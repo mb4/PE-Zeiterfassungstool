5d12e91974df61491255b52c1701e3356c971fb9
Stephan <stegiemail-github@yahoo.de>
2014-09-23 16:03:50 +0200

    functions generateUniqueId, createInternship
    Change-Id: I8c2856ec9c31809c2a53d41ed531bfec5322ca6d
    Signed-off-by: Stephan <stegiemail-github@yahoo.de>

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,12 @@
+/* * * * * * * * * * * * * * * * * * * * * *
+ * 										   *
+ * TimeTrackTool						   *
+ * 										   *
+ * 2014 (c) Marvin Botens, Stephan Giesau  *
+ * 										   *
+ * * * * * * * * * * * * * * * * * * * * * */
+
+ 
 // load DB
 // initialise. If the database doesn't exist, it is created
 var db = new localStorageDB('timetracktool', localStorage);
@@ -16,3 +25,40 @@ if( db.isNew() ) {
 
 //TODO remove, console output of DB contents
 console.debug( JSON.parse( db.serialize() ) );
+
+
+
+/**
+ * generate a unique id with given timestamp, salt and browser's user agent
+ * @param timestamp timestamp of object
+ * @param salt string used to salt the generated key
+ * @return string containing a MD5 hash usable as ID
+ */
+function generateUniqueId(timestamp, salt) {
+	
+	return calcMD5( timestamp + salt + navigator.userAgent );
+}
+
+
+
+/**
+ * create new internship in database
+ * 
+ * @return unique_id of new entry
+ */
+function createInternship(f_name, f_start, f_end, f_manager, f_lerner_id) {
+	
+	f_unique_id = generateUniqueId(f_start, f_name);
+
+	db.insert('internship', {
+			unique_id: f_unique_id,
+			name: f_name,
+			start: f_start,
+			end: f_end,
+			manager: f_manager,
+			lerner_id: f_lerner_id
+		});
+	db.commit();
+	
+	return f_unique_id;
+}
