4f02f728ee1bbe089019ce0ced552665101ee4f2
Stephan <stegiemail-github@yahoo.de>
2014-09-23 18:51:29 +0200

    Manual merge of custom.js
    Change-Id: I4da0959a1763ceadb29ceb7fe40037162a313185
    Signed-off-by: Stephan <stegiemail-github@yahoo.de>

--- js/custom.js
+++ js/custom.js
@@ -6,9 +6,13 @@
  * 										   *
  * * * * * * * * * * * * * * * * * * * * * */
 
- 
-// load DB
-// initialise. If the database doesn't exist, it is created
+
+
+/////////////////////////////////////////////
+// DATABASE INITIALIZATION                 //
+/////////////////////////////////////////////
+
+// initialise DB. If the database doesn't exist, it is created
 var db = new localStorageDB('timetracktool', localStorage);
 
 // check if the database was just created. Then create all tables
@@ -27,6 +31,12 @@ if( db.isNew() ) {
 console.debug( JSON.parse( db.serialize() ) );
 
 
+
+
+
+/////////////////////////////////////////////
+// BASIC FUNCTIONS                         //
+/////////////////////////////////////////////
 
 /**
  * generate a unique id with given timestamp, salt and browser's user agent
@@ -105,3 +115,83 @@ function createInternship(f_name, f_start, f_end, f_manager, f_lerner_id, f_dail
 
 //createOrUpdateDay(222, 2355, "Holiday");
 //createInternship("test", 234, 445, "df", "fd", 2443, 222);
+
+
+
+
+
+/////////////////////////////////////////////
+// APPLICATION STARTUP                     //
+/////////////////////////////////////////////
+
+//add datepickers to form
+$('#form-internship-start').datepicker();
+$('#form-internship-end').datepicker();
+
+
+
+
+
+/////////////////////////////////////////////
+// EVENT HANDLERS                          //
+/////////////////////////////////////////////
+
+//edit internship button handlers
+$('#edit-internship-button').on('click', function() {
+
+	// fill form with internship data
+	// TODO
+	// TODO get uniqueId of currently opened internship
+	// TODO load data into form elements (input via element ID)
+	// TODO set values of data-date attribute for datepickers
+
+	// open modal with form
+	$('#form-internship').modal();
+});
+
+//create internship button handlers
+$('#edit-internship-button').on('click', function() {
+	
+	// open modal with form
+	$('#form-internship').modal();
+});
+
+
+//handler for tracking start/stop
+$('#tracking-button').on('click', function(e) {
+	
+	button = $('#tracking-button');
+
+	// start tracking
+	if(button.hasClass('btn-success')) {
+
+		// change text and color of button
+		button.addClass('btn-danger').removeClass('btn-success').find('strong').text('Stop tracking');
+		
+		// start timer for tracking 
+		// TODO
+		$('#tracking-time').text('Start');
+	
+	// stop tracking
+	} else {
+		
+		// change text and color of button
+		button.addClass('btn-success').removeClass('btn-danger').find('strong').text('Start tracking');
+		
+		// stop timer for tracking
+		// TODO
+		$('#tracking-time').text('Stop');
+		
+		// save to working_period
+		// TODO
+		
+		// add working period to day overview (if displayed day there is current day)
+		// TODO
+		
+	}
+});
+
+
+
+
+// END TODO remove after finishing work
