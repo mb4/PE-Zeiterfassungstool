c774f47b59f182f7d2c84e5af420ce58dbc06605
Stephan Giesau <stephan.giesau@de.ibm.com>
2014-11-28 09:58:15 +0100

    Revert "Fix #11, add buttons for vacation/holiday in internship modal"
    This reverts commit a9f7c92c5a0bce1e52070be8253c4259291bf392.

--- js/frontend.js
+++ js/frontend.js
@@ -839,17 +839,15 @@ $('#form-internship-delete').on('click', function() {
 // btn-add-dynblock handler (adds vacation period or holiday to internship form)
 $('.btn-add-dynblock').on('click', function(e) {
    
-	e.preventDefault();
-	
-	var type = $(this).attr("data-type");
-   	//var id = $("#dynblock-wrapper > div").length; //TODO: remove
+   var type = $(this).attr("data-type");
+   //var id = $("#dynblock-wrapper > div").length; //ToDo: remove
    
-	addDynblock(type);
+   addDynblock(type);
    
-	/*$("#dynblock-wrapper").prepend(getDynblock(id, type));
-	$('#form-internship-dynblock-col1-'+id).datepicker();
-	if (type == "Vacation") $('#form-internship-dynblock-col2-'+id).datepicker();
-	*/ //TODO remove
+   /*$("#dynblock-wrapper").prepend(getDynblock(id, type));
+   $('#form-internship-dynblock-col1-'+id).datepicker();
+   if (type == "Vacation") $('#form-internship-dynblock-col2-'+id).datepicker();
+   */ //TODO remove
 });
 
 
